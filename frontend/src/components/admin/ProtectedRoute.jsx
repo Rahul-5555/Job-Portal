daa7f019685721c3f4127e85fd8a2e3e81027e25
Rahul-5555 <rahul.malviya676@gmail.com>
2024-09-08 22:35:09 +0530

    Deployement

--- frontend/src/components/admin/ProtectedRoute.jsx
+++ frontend/src/components/admin/ProtectedRoute.jsx
@@ -2,21 +2,22 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-const ProtectedRoute = ({children}) => {
-    const {user} = useSelector(store=>store.auth);
-
+const ProtectedRoute = ({ children }) => {
+    const { user } = useSelector(store => store.auth);
     const navigate = useNavigate();
 
-    useEffect(()=>{
-        if(user === null || user.role !== 'recruiter'){
+    useEffect(() => {
+        if (user === null || user.role !== 'recruiter') {
+            console.log("Redirecting to home...");
             navigate("/");
         }
-    },[]);
+    }, [user, navigate]); // Ensure the effect runs when `user` changes
 
     return (
         <>
-        {children}
+            {user && user.role === 'recruiter' ? children : null}
         </>
-    )
+    );
 };
+
 export default ProtectedRoute;
