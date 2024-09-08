22a3ae99f15303df1d7a42e2298f7a2a4c2841cf
Rahul-5555 <rahul.malviya676@gmail.com>
2024-09-08 18:04:34 +0530

    Deployement

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,31 @@
 import express, { urlencoded } from "express";
-import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser"
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
-
-dotenv.config();
+import userRoute from "./routes/user.route.js"
+import companyRoute from "./routes/company.route.js"
+import jobRoute from "./routes/job.route.js"
+dotenv.config({});
+import applicationRoute from "./routes/application.route.js"
 
 const app = express();
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+
+// middleware
+app.use(express.json()); // sending data in json format
+app.use(express.urlencoded({ extended: true })) // midd
 app.use(cookieParser());
-
-const allowedOrigins = ["https://jobportalrah.netlify.app", "http://localhost:5173"];
-
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: "https://jobportalrah.netlify.app",
   credentials: true
-};
-
+}
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
-// APIs
+// api's 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
@@ -42,5 +33,5 @@ app.use("/api/v1/application", applicationRoute);
 
 app.listen(PORT, () => {
   connectDB();
-  console.log(`Server running at port ${PORT}`);
-});
+  console.log(`Server running at port ${PORT}`)
+})
